44224ca8627ca924dc735cd09519b237b33d49d9
Michael The <mcgthe@gmail.com>
2013-03-21 16:19:23 +0100

    Added base template

---
+++ app/controllers/TempBase.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.Controller
+import play.api.mvc.Action
+import play.api.data.Forms._
+
+import views._
+
+object TempBase extends Controller {
+	
+	def hello() = Action {
+	  Ok(html.base("TITLE LIKES CAPS LOCK"))
+	}
+}
