27d76c78c7fa3a6f68ee600a2bde3c1550ae2474
Michael The <mcgthe@gmail.com>
2013-03-22 16:32:38 +0100

    Added extra pages

--- app/controllers/TempBase.scala
+++ app/controllers/TempBase.scala
@@ -3,12 +3,13 @@ package controllers
 import play.api.mvc.Controller
 import play.api.mvc.Action
 import play.api.data.Forms._
-
 import views._
+import play.api.templates.Html
 
 object TempBase extends Controller {
 	
 	def hello() = Action {
-		Ok(html.base("TITLE LIKES CAPS LOCK"))
+		Ok(html.base("TITLE LIKES CAPS LOCK")(Html.apply("Some placeholder text")))
 	}
+	
 }
