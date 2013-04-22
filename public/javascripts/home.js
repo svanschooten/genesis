58467e5e9bd004fa84b71039b8a37ab91bf4f17d
Michael The <mcgthe@gmail.com>
2013-04-22 14:25:10 +0200

    Working addTab/closeTab func

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,31 +1,38 @@
 function addTab() {
-	var id = prompt("Please enter a name for your circuit");
-					
-	$('.tab-content').append(
-		$('<div>').attr({
-			id: 'content-'+id,
-		}).addClass('tab-pane').text(id)
-	);
+  var id = prompt("Please enter a name for your circuit");
+				
+  $('.tab-content').append(
+    $('<div>')
+      .attr('id', 'content-'+id)
+      .addClass('tab-pane')
+      .text('placeholder text')
+  );
 
-	var close_button = "<button class=\"close\" type=\"button\" onclick=\"closeTab(this)\">&times;</button>";
+  var close_button = $('<button>')
+    .addClass('close')
+    .attr({
+      type: 'button',
+      onclick: 'closeTab(this)'
+    })
+    .html('&times;')[0].outerHTML;
+
+  var tab_link = $('<a>')
+        .attr({
+          href: '#content-'+id,
+          'data-toggle': 'tab'
+        })
+        .html(id +" "+ close_button);
+
+  var tab = $('<li>')
+    .attr('id', id)
+    .html(tab_link);
 	
-	var tab = $('<li>').attr({
-				id: id
-			}).html(
-				$('<a>').attr({
-					href: '#'+id,
-					'data-toggle': 'tab'
-				}).html(id +" "+ close_button)
-	);
-	
-	tab.insertBefore($('#add-tab-button'));
-	
-	$('#myTab a:last').tab('show');
+  tab.insertBefore($('#add-tab-button'));	
+  $('#myTab a:last').tab('show');
 }
 
 function closeTab(e) {
-alert($(e).parent().id);
-//	var id = e.id;
-//	$('#'+id).remove();
-//	$('#content-'+id).remove();
+	var id = $(e).parent().parent()[0].id;
+	$(e).parent().parent().remove();
+	$('#content-'+id).remove();
 }
