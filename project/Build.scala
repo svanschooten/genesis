efabd330c2a856fcf8e2cfe002e76e77f649cb67
pabouter <P.A.Bouter@student.tudelft.nl>
2013-04-25 09:32:04 +0200

    about to pull ODE solver

--- project/Build.scala
+++ project/Build.scala
@@ -13,9 +13,8 @@ object ApplicationBuild extends Build {
     anorm
   )
 
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
   )
 
 }
