ae3da1b01bbe0c35720bf0d2269e8f0fe1ea1478
Jeroen Peperkamp <sharky.suit@gmail.com>
2013-04-25 11:15:36 +0200

    fixed gitignore to ignore scalation, optimized ode solving code

--- app/models/rungekuttatest.scala
+++ app/models/rungekuttatest.scala
@@ -62,12 +62,14 @@ object Rungekuttatest {
     RungeKutta.integrateVV (odes, cVec, dt)
   }
 
-  /** Does not work yet...
-   * @param t
-   * @param dt
-   * @param odes
-   * @param cVec
-   * @return
+  /** Solve a list of ODEs, given start concentrations and time span and return a list of
+   * vectors with the concentrations at each point in time; time is sliced according to the
+   * step size
+   * @param t The final time (time span will be [0.0, t])
+   * @param dt The step size
+   * @param odes The list of ODE functions
+   * @param cVec The initial concentrations
+   * @return The list of concentration at each time increment 0.0+dt*i
    */
   def solveFolding(t: Double, dt: Double, odes: Array [DerivativeV], cVec: VectorD): List[VectorD] = {
     (0.0 to t by dt).toList.foldLeft(List[VectorD]())((l: List[VectorD], step: Double) => l match {
@@ -85,15 +87,10 @@ object Rungekuttatest {
   * @return The list of concentrations.
   */
   def solveRecursive(time: Double, dt: Double, odes: Array [DerivativeV], cVec: VectorD): List[VectorD] = {
-    if (time / dt > 1000.0) {
-      throw new IllegalArgumentException("Resolution too high, provide smaller step size.")
-    }
-    if (time <= 0.0) {
+    require(math.abs(time / dt) > 1000.0,"Resolution too high, provide smaller step size.")
+    if (time <= 0.0)
       List()
-    } else {
-      val res = solveSingle(odes, cVec, dt)
-      res :: solveRecursive(time - dt, dt, odes, res.clone())
-    }
+    else
+      solveSingle(odes, cVec, dt) :: solveRecursive(time - dt, dt, odes, res.clone())
   }
 }
-
