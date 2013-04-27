531b48ef4c9be56310f1d4731cffe19cbf0eaae9
Jeroen Peperkamp <sharky.suit@gmail.com>
2013-04-27 16:17:31 +0200

    updated model and finished solver to solve skipped CSs as well

--- app/factories/ODEFactory.scala
+++ app/factories/ODEFactory.scala
@@ -57,7 +57,7 @@ object ODEFactory {
      *  If a CodingSeq is encountered, no ODE needs to be generated and None is returned
      */
     def mkTuple(part: Part): Option[ODEPair] = part match {
-        case NotGate(CodingSeq(_, _, (_,c_in), _, _), CodingSeq(k2, (d1, d2), (c_out_r,c_out_p), _, _), k1, km, n) => Some(
+        case NotGate(CodingSeq(_, _, (_,c_in), _), CodingSeq(k2, (d1, d2), (c_out_r,c_out_p), _), k1, km, n) => Some(
             //concs(0):[TF]; concs(1): [mRNA]; concs(2): [Protein]
             (   (time: Double, concs: VectorD) => new VectorD(Array(
                     (k1 * km ~^ n) / (km ~^ n + concs(0) ~^ n) - d1 * concs(1),
@@ -66,7 +66,7 @@ object ODEFactory {
                 new VectorD(Array(c_in, c_out_r, c_out_p))
             )
         )
-        case AndGate((CodingSeq(_, _, (_,c_in_1), _, _), CodingSeq(_, _, (_,c_in_2), _, _)), CodingSeq(k2, (d1, d2), (c_out_r,c_out_p), _, _), k1, km, n) => Some(
+        case AndGate((CodingSeq(_, _, (_,c_in_1), _), CodingSeq(_, _, (_,c_in_2), _)), CodingSeq(k2, (d1, d2), (c_out_r,c_out_p), _), k1, km, n) => Some(
             // concs(0): [TF1]; concs(1): [TF2]; concs(2): [mRNA]; concs(3): [Protein]
             (   (time: Double, concs: VectorD) => new VectorD(Array(
                     (k1 * (concs(0) * concs(1)) ~^ n) / (km ~^ n + (concs(0) * concs(1)) ~^ n) - d1 * concs(2),
@@ -86,31 +86,49 @@ object ODEFactory {
      *  in the current list, must have both of its inputs in the list. If it doesn't,
      *  the gate belongs to a different, deeper level and will be handled later,
      *  when both of its inputs have been updated.
-     *  @param firstCSs The list of coding sequences that form the input to the network
+     *  @param firstCSs The CSs that form the inputs to the network
      */
     def step(firstCSs: List[CodingSeq]) {
-        currentTime += stepSize
         // TODO the concentrations of the CodingSeqs at the beginning of the network may need to be updated separately; ask Alexey
         // TODO whatever code loops to call step a number of times must reset the ready booleans on the CSs before each step
         // TODO also, when the simulation is started, the currentTime (and possibly step size) must be reset
+        currentTime += stepSize
 
-        // filter out the CodingSeqs we're going to mess with on this level
-        val cs_partition = firstCSs.partition(cs => cs match {
-            case CodingSeq(_,_,_,link,_) => link match {
+        // function filter out the CodingSeqs we're going to mess with on some level
+        def partition(css: List[CodingSeq]): (List[CodingSeq],List[CodingSeq]) = css.partition( _ match {
+            case CodingSeq(_,_,_,link) => link match {
                 case Some(cx: NotGate) => true
                 case Some(AndGate((seq1,seq2),_,_,_,_)) if firstCSs.contains(seq1) && firstCSs.contains(seq2) => true
                 case _ => false
             }
             case _ => false
         })
-        // generate the appropriate ODEPairs and update the concentrations
-        val parts = cs_partition._1.collect( { case CodingSeq(_,_,_,Some(link),_) => link } )
-        val odePairs = mkODEs(parts)
-        val results = solve(odePairs)
-        results.zip(parts).foreach({
-            case (a,b:NotGate) => b.output.concentration=(a(0),a(1)); b.output.ready=true
-            case (a,b:AndGate) => b.output.concentration=(a(0),a(1)); b.output.ready=true
-            })
+
+        do_the_math((firstCSs,List[CodingSeq]()))
+
+        // the function that will do the actual work
+        def do_the_math(css: (List[CodingSeq],List[CodingSeq])) {
+            // first figure out which CSs can be updated on this level
+            val newPartition = partition(css._1)
+            val oldPartition = partition(css._2)
+            val goodCSs = newPartition._1 ::: oldPartition._1
+            val badCSs = newPartition._2 ::: oldPartition._2
+            
+            // then generate the appropriate ODEPairs and update the concentrations
+            val parts = goodCSs.collect( { case CodingSeq(_,_,_,Some(link)) => link } )
+            val odePairs = mkODEs(parts)
+            val results = solve(odePairs)
+            results.zip(parts).foreach({
+                case (a,b:NotGate) => b.output.concentration=(a(0),a(1))
+                case (a,b:AndGate) => b.output.concentration=(a(0),a(1))
+                })
+            // finally, recursively update the rest of the network using the next
+            // CSs after the gates we updated, passing along the ones we didn't touch yet
+            do_the_math((parts.collect( {
+                case NotGate(_, out, _, _, _) => out
+                case AndGate(_, out, _, _, _) => out } ), badCSs))
+        }
+
     }
 
     /**
