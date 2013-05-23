32937e710e9226f00260ba0f55e0f474f61a193d
Stijn van Schooten <s.vanschooten@student.tudelft.nl>
2013-05-23 11:49:25 +0200

    updated the parser

--- public/javascripts/rkPlot.js
+++ public/javascripts/rkPlot.js
@@ -29,53 +29,6 @@ function parseJSONdata(response){
        data = $.parseJSON(response);
     }
 
-    //Parse the different vectors from the JSON object
-    var time = data["t"];
-    var vectors = data["vectors"];
-    var names = data["names"];
-
-    //Instantiate a new color pallette
-    var palette = new Rickshaw.Color.Palette( { interpolatedStopsCount: vectors[0].length } );
-
-    //Make the data array, now still empty
-    var series = new Array();
-
-    //Fill the data array
-    for (var i=0;i<vectors[0].length;i++){
-        //Make new series object
-        var serie = new Object();
-        //Give it a name
-        serie.name = names[i];
-        //Instantiate the data object in memory
-        var sData = [];
-        //Fill the data object
-        for (j=0;j<time.length-1;j++){
-            var value = vectors[j][i];
-            sData.push({x: (time[j] * 1000), y: value});
-            if(max_c_x < value){
-                max_c_x = value
-            }
-        }
-        //Add data an color
-        serie.data = sData;
-        serie.color = palette.color();
-        //Push data into the data array
-        series.push(serie);
-    }
-
-    return series;
-}
-
-/**
-Parses the standard JSON ouput to a usable format for plotting.
-
-function parseJSONdata(response){
-
-    //Check if data in memory is empty
-    if(data == null) {
-       data = $.parseJSON(response);
-    }
-
     //Instantiate a new color pallette
     var palette = new Rickshaw.Color.Palette( { interpolatedStopsCount: data.length } );
 
@@ -86,7 +39,7 @@ function parseJSONdata(response){
     }
 
     return data;
-}*/
+}
 
 /**
 Plots the received data in a interactive plot.
