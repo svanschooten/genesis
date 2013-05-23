09444ee75801773657a45cb886786c5921f4d807
Michael <mcgthe@gmail.com>
2013-05-23 09:30:11 +0200

    Added test page for home, start new home page

--- app/controllers/Home.scala
+++ app/controllers/Home.scala
@@ -7,12 +7,17 @@ import play.api.templates.Html
 import models._
 import views._
 
-object Home extends Controller with Secured{
-  
+object Home extends Controller with Secured {
+
   val homeText = "Some text that'll make you feel right at home"
-    def home = IsAuthenticated { email => _ =>
-    User.findByEmail(email).map { user =>
-    	Ok(html.home("Home")(Html.apply(homeText)))
-    }.getOrElse(Forbidden)
+    
+  def home = IsAuthenticated { email => _ =>
+      User.findByEmail(email).map { user =>
+        Ok(html.home("Home")(Html.apply(homeText)))
+      }.getOrElse(Forbidden)
+  }
+  
+  def hometest = Action { implicit request =>
+    Ok(html.home("Home")(Html.apply("Some content")))
   }
 }
