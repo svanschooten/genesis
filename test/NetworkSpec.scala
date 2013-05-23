62d99123c10c9748bd1ccd83aa9d94ac36568cf3
Jeroen Peperkamp <sharky.suit@gmail.com>
2013-05-23 09:12:56 +0200

    fixed test expected values

--- test/NetworkSpec.scala
+++ test/NetworkSpec.scala
@@ -11,7 +11,7 @@ class NetworkSpec extends Specification {
 
     "Network" should {
         "return correct results" in new WithApplication {
-            val concs = (0.0 to 1.0 by 0.001).toList
+            val concs = List.fill(1503)(1.0)
             val A = CodingSeq("A",concs.zip(concs), true)
             val B = CodingSeq("B",concs.zip(concs),isInput=true)
             val C = CodingSeq("C",isInput=false)
@@ -22,28 +22,23 @@ class NetworkSpec extends Specification {
             val notAandB = AndGate((C,B),D)
             val notAandBandE = AndGate((D,E),F)
             val net = new Network(List(A,B,E))
-            val results = net.simulate(5.0)
+            val results = net.simulate(1500.0)
             // expected results generated using MATLAB R2012b
             //._2 = mRna, ._3 = protein
-            // B: 48.7804878048759	260.792124555058
-            // D: 334.934010152227	1279.12460265271
-            // F: 271.331210172459	941.508188692855
-            // A: 41.6666666666666	228.054374360186
-            // E: 61.3496932500570	148.920900749709
-            // C: 99.7682070794492	497.584659558515
-            //println(results(results.length-1))
-            results(results.length-1)(0)._2 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(0)._3 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(1)._2 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(1)._3 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(2)._2 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(2)._3 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(3)._2 must beCloseTo(201.331, 0.01)
-            results(results.length-1)(3)._3 must beCloseTo(991.809, 0.01)
-            results(results.length-1)(4)._2 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(4)._3 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(5)._2 must beCloseTo(0.0, 0.01)
-            results(results.length-1)(5)._3 must beCloseTo(0.0, 0.01)
+            // B: 1 1
+            // D: 334.730082600319	1278.34579625832
+            // F: 270.671535349476	939.211356776036
+            // A: 1 1
+            // E: 1 1
+            // C: 200.437196650108	999.661888066052
+            results(results.length-1).foreach(t => t._1 match {
+                case "A" => t._2 must beCloseTo(1.0,0.1); t._3 must beCloseTo(1.0,0.1)
+                case "B" => t._2 must beCloseTo(1.0,0.1); t._3 must beCloseTo(1.0,0.1)
+                case "C" => t._2 must beCloseTo(200.44,0.1); t._3 must beCloseTo(999.66,0.1)
+                case "D" => t._2 must beCloseTo(334.73,0.1); t._3 must beCloseTo(1278.35,0.1)
+                case "E" => t._2 must beCloseTo(1.0,0.1); t._3 must beCloseTo(1.0,0.1)
+                case "F" => t._2 must beCloseTo(270.67,0.1); t._3 must beCloseTo(939.21,0.1)
+            })
         }
     }
 }
