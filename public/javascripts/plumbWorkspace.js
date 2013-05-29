0531f8a537fccaaf53168cf6a4cf4b7aaf39e74c
Stijn van Schooten <s.vanschooten@student.tudelft.nl>
2013-05-29 22:49:26 +0200

    added protein selection to modal.

--- public/javascripts/plumbWorkspace.js
+++ public/javascripts/plumbWorkspace.js
@@ -23,6 +23,7 @@ var dropOptions = {
 };
 
 var jsp;
+var currentConnection = null;
 
 
 /**
@@ -52,13 +53,22 @@ function findElement(array, elementId) {
     return null;
 }
 
-function setProtein(connection) {
+function openProteinModal(connection){
+    currentConnection = connection;
+    proteinModal.modal("show");
+}
+
+function setProtein() {
 
     //TODO Protein selectie en controle
-    var protein = prompt("What protein do you want to bind?");
-    connection.protein = protein;
-    connection.removeOverlay("label");
-    connection.addOverlay([ "Label", {label:protein, location: 0.25, cssClass: "aLabel", id:"label"}]);
+    if(selectedProtein == null){
+        alertError("Invalid protein selection!");
+        return;
+    }
+    currentConnection.protein = selectedProtein;
+    currentConnection.removeOverlay("label");
+    currentConnection.addOverlay([ "Label", {label: selectedProtein.name, location: 0.3, cssClass: "aLabel", id:"label"}]);
+    proteinModal.modal("hide");
 }
 
 function makeConnection(params) {
@@ -66,18 +76,15 @@ function makeConnection(params) {
         notify("Cannot connect to self.", "Warning");
         return false;
     }
-    var confirmed = confirm("Connect " + params.sourceId + " to " + params.targetId + "?");
-    if(confirmed) {
-        var element = findElement(circuit, params.sourceId.replace("#",""));
-        if(element == null) {
-            notify("Invalid element: " + params.sourceId, "Warning");
-        } else {
-            params.connection.protein = "";
-            params.connection.addOverlay([ "Arrow", { width:15, location: 0.5,height:10, id:"arrow" }]);
-            params.connection.bind("click", function(connection){ setProtein(connection) });
-        }
+    var element = findElement(circuit, params.sourceId.replace("#",""));
+    if(element == null) {
+        notify("Invalid element: " + params.sourceId, "Warning");
+        return false;
     }
-    return confirmed;
+    params.connection.protein = "";
+    params.connection.addOverlay([ "Arrow", { width:15, location: 0.7,height:10, id:"arrow" }]);
+    params.connection.bind("click", function(connection){ openProteinModal(connection) });
+    return true;
 }
 
 /**
