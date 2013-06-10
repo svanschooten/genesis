51bd438af66f94a3aed15d277e8d78f4f0e9890d
edward <edward_da_indo@hotmail.com>
2013-06-10 11:21:13 +0200

    seleniumSpec verbeterd

--- test/SeleniumSpec.scala
+++ test/SeleniumSpec.scala
@@ -14,7 +14,6 @@ import org.openqa.selenium.interactions._
  */
 class SeleniumSpec  extends Specification {
 
-  case class param(sourceId : String, targetId:String)
   implicit val webDriver : WebDriver = new FirefoxDriver
 
   "The blog app home page" should {
@@ -22,7 +21,8 @@ class SeleniumSpec  extends Specification {
       /* 
        * Testing url with wrong information
        */
-      webDriver.get("http://80.112.151.137:9001")
+      //webDriver.get("http://80.112.151.137:9001")
+      webDriver.get("http://127.0.0.1:9000")
       webDriver.findElement(By.id("email")).sendKeys("wrong@email.com")
       webDriver.findElement(By.id("password")).sendKeys("helloworld")
       webDriver.findElement(By.id("loginbutton")).click()
@@ -59,12 +59,50 @@ class SeleniumSpec  extends Specification {
       assertTrue(webDriver.findElements(By.id("input")).size() > 0)
       assertTrue(webDriver.findElements(By.id("output")).size() > 0)
       val builder : Actions = new Actions(webDriver)
+      val gate : WebElement = webDriver.findElement(By.id("ng")) 
       val input : WebElement = webDriver.findElement(By.id("input"))
       val output : WebElement = webDriver.findElement(By.id("output"))
-      val dragAndDrop : Action = builder.clickAndHold(input).moveToElement(output).click().build()
-      dragAndDrop.perform()
+      val dragNdrop : Action = builder.clickAndHold(gate).moveToElement(output).click().build()
+      dragNdrop.perform()
+      assertTrue(webDriver.findElements(By.id("not2")).size() > 0)
+      val makeConnection : Action = builder.clickAndHold(input).moveToElement(output).click().build()
+      makeConnection.perform()
       val svg : WebElement = webDriver.findElement(By.className("_jsPlumb_connector"))
       assertTrue(webDriver.findElements(By.className("_jsPlumb_connector")).size() > 0)
+      
+      /*
+       * Testing new (clean workspace)
+       */
+      webDriver.findElement(By.id("circuit")).click()
+      webDriver.findElement(By.id("new")).click()
+      val alert : Alert = webDriver.switchTo().alert()
+      alert.accept()
+      webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+      webDriver.findElement(By.className("btn-primary")).click()
+     
+      /*
+       * Testing load 
+       */
+      webDriver.findElement(By.id("circuit")).click()
+      webDriver.findElement(By.id("load")).click()
+      webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+      webDriver.findElement(By.id("loadNetworkSelector")).sendKeys("NotGate")
+      webDriver.findElement(By.id("loadcircuit")).click()
+      webDriver.findElement(By.id("loadcircuit")).click()
+      assertTrue(webDriver.findElements(By.id("not2")).size() > 0)
+      assertTrue(webDriver.findElements(By.className("_jsPlumb_connector")).size() > 0)
+      
+      /*
+       * Testing simulate
+       */
+      webDriver.findElement(By.id("simulation")).click()
+      webDriver.findElement(By.id("runcircuit")).click()
+      webDriver.findElement(By.id("signalArea")).sendKeys("wrong@email.com")
+      
+      /*
+       * Testing save
+       */
+      
       
       /*
        * Testing logout
